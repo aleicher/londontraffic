28ad5ccfdf1d79681842764e7aedc6b22c7011dd
Andreas Leicher <andreas.leicher@mapcase.de>
2015-01-17 17:31:54 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Londontraffic::Application.config.secret_key_base = 'c97eb9f4d3352ad9626d0e58436c53ff343a9508d4603b44d7de0a276de38682a04430016f9926f1fc789f507713c965e8687a9c8590e1f97f09187a6a8e4ec0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Londontraffic::Application.config.session_store :cookie_store, key: '_londontraffic_session'
