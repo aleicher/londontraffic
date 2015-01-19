2501522cfb420e4069a840b7b7fcb5d24b94ad2e
Andreas Leicher <andreas.leicher@mapcase.de>
2015-01-19 23:04:20 +0100

    let unicorn spawn sidekiq
    this way sidekiq does not require an additional worker on heroku

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,3 +2,15 @@ worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
 timeout 15
 preload_app true
 
+before_fork do |server, worker|
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
+
+after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+end
