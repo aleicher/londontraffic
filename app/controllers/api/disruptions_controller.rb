823f76d332e364245c76bd77f70cdd7e71baede0
Andreas Leicher <andreas.leicher@mapcase.de>
2015-01-19 23:17:16 +0100

    disruptions_controller has show action

--- app/controllers/api/disruptions_controller.rb
+++ app/controllers/api/disruptions_controller.rb
@@ -4,6 +4,11 @@ class Api::DisruptionsController < Api::ApplicationController
     respond_with :api, :v1, disruptions
   end
 
+  def show
+    disruption = disruption_repo.disruption_for_id!(params[:id])
+    respond_with :api, :v1, disruption
+  end
+
   private
 
   def disruption_repo
