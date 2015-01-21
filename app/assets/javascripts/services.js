888ed2e844ba9d5acf2e4ae570a9d88b5c411a20
Andreas Leicher <andreas.leicher@mapcase.de>
2015-01-21 10:27:14 +0100

    wrap angular DI for services

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,16 +1,18 @@
 angular.module('trafficApp.services', [])
-  .service('DisruptionsSvc', function($http, $q) {
-    var apiUrl = "/api/v1/disruptions";
-    this.getDisruptions = function() {
-      var deferred = $q.defer();
-      $http({
-        method: 'GET',
-        url: apiUrl
-      }).success(function(data) {
-        deferred.resolve(data);
-      }).error(function() {
-        deferred.reject('There was an error');
-      });
-      return deferred.promise;
-    };
-  });
+  .service('DisruptionsSvc', ['$http', '$q',
+    function($http, $q) {
+      var apiUrl = "/api/v1/disruptions";
+      this.getDisruptions = function() {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url: apiUrl
+        }).success(function(data) {
+          deferred.resolve(data);
+        }).error(function() {
+          deferred.reject('There was an error');
+        });
+        return deferred.promise;
+      };
+    }
+  ]);
